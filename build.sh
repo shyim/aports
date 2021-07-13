39ec84bf450091bca86b7f4beb5d31cc7709373e
Shyim <6224096+shyim@users.noreply.github.com>
2021-07-13 21:40:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ echo "http://dl-cdn.alpinelinux.org/alpine/${ALPINE_VERSION}/main" | sudo tee /e
 sudo apk update
 sudo apk add --upgrade apk-tools
 sudo apk upgrade --available
-git clone https://github.com/shyim/aports.git /tmp/aports --depth=1
 cd /tmp/aports/community/gnu-libiconv
 abuild -r
-cp /home/buildozer/packages/community/x86_64/* /home/buildozer/out/
+cp /home/buildozer/packages/community/*/*.apk /home/buildozer/out/
